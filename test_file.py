eeaa60ce83f730a8b1b8309444d1f811d5eb9d46
Max Kristian Dunmire <maxkdunmire@gmail.com>
2025-03-21 22:15:50 -0700

    added api authentication

---
+++ test_file.py
@@ -0,0 +1,49 @@
+import unittest
+import titanic_ml
+from pathlib import Path
+
+class TestAPIConfig(unittest.TestCase):
+    '''Testing class for the api finding the kaggle folder containing api key'''
+    def test_api_setup(self):
+        '''Will test that the authentication of the kaggle api using alternate folder is functional.'''
+
+        # import os for kaggle.json folder configuration
+        import os
+        os.environ['KAGGLE_CONFIG_DIR'] = r"C:\Users\maxdu\.kaggle\kaggle_private"
+
+        passed = True
+        error = None
+
+        try:
+            # import kaggle api
+            import kaggle
+            from kaggle.api.kaggle_api_extended import KaggleApi
+            api = KaggleApi()
+            api.authenticate()
+        except OSError as e:
+            error = e
+            passed = False
+
+        self.assertTrue(passed, error)
+
+class TestDownloadDatasets(unittest.TestCase):
+    '''Testing class for the download_dat() function'''
+    def setUp(self):
+        '''Authenticate the api before downloading'''
+        titanic_ml.kaggle.api.authenticate()
+
+    def test_file_creation(self):
+        '''will test that the proper file names are created in the right folder'''
+        titanic_ml.download_data()
+
+        train_path = Path("./data/train.csv")
+        test_path = Path("./data/test.csv")
+
+        self.assertTrue(train_path.exists())
+        self.assertTrue(test_path.exists())
+
+    def test_file_contents(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
--- titanic_ml.py
+++ titanic_ml.py
@@ -1,1 +1,21 @@
-# this is a test file
+'''This program will create and train a model for the Titanic kaggle competition'''
+# configure API Credentials path befor kaggle import
+import os
+os.environ['KAGGLE_CONFIG_DIR'] = r"C:\Users\maxdu\.kaggle\kaggle_private"
+
+import kaggle
+
+# authenticate api
+kaggle.api.authenticate()
+
+def download_data():
+    # download seperately, as opposed to competition_download_files
+    # because that will create a zip folder, and there is no unzip option
+    kaggle.api.competition_download_file(competition="titanic", file_name="train.csv", path='./data')
+    kaggle.api.competition_download_file(competition="titanic", file_name="test.csv", path="./data")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
